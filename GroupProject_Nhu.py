f3013c91da20ef3e9228371a305a353ec31065da
Katie_Nguyen <nguyenngocquynhnhu1710@gmail.com>
2023-12-06 21:01:05 -0500

    Update

--- GroupProject_Nhu.py
+++ GroupProject_Nhu.py
@@ -8,6 +8,17 @@ Created on Wed Nov 29 08:30:21 2023
 import numpy as np
 import pandas as pd
 import os
+import nltk 
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix, accuracy_score
+stop_words = set(stopwords.words('english'))
+
 path = "D:/MyCentennial/COMP237_IntroductionToAI/"
 filename = 'Youtube01-Psy.csv'
 fullpath = os.path.join(path,filename)
@@ -24,17 +35,6 @@ data.dtypes
 data.head(5)
 
 # Text preprocessing
-import nltk 
-from nltk.corpus import stopwords 
-from nltk.tokenize import word_tokenize
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import confusion_matrix
-stop_words = set(stopwords.words('english'))
-
 count_vectorizer = CountVectorizer()
 train_tc = count_vectorizer.fit_transform(data['CONTENT'])
 print("\nDimensions of training data:", train_tc.shape)
@@ -47,23 +47,19 @@ type(train_tfidf)
 data = data.sample(frac=1)
 
 # Split dataset into training and testing sets
-train_size = int(0.75 * len(data))
-train_data = data[:train_size]
-test_data = data[train_size:]
+train_size = int(0.75 * data.shape[0])
+X_train, X_test = train_tfidf[:train_size], train_tfidf[train_size:]
+y_train, y_test = data['CLASS'][:train_size], data['CLASS'][train_size:]
 
 # Fit the training data into a Naive Bayes classifier
-clf = MultinomialNB().fit(train_tfidf, data['CLASS'])
+clf = MultinomialNB().fit(X_train, y_train)
 
 # Cross validate the model on the training data using 5-fold
-scores = cross_val_score(clf, train_tfidf, data['CLASS'], cv=5)
+scores = cross_val_score(clf, X_train, y_train, cv=5)
 print("Cross validation score: ", scores)
-print("Mean cross validation score: ", np.mean(scores))
+print("Mean cross validation score: ", scores.mean())
 
 # Test the model on the test data
-X_test_counts = count_vectorizer.transform(test_data['CONTENT'])
-X_test_tfidf = tfidf.transform(X_test_counts)
-predicted = clf.predict(X_test_tfidf)
-
-# Print the confusion matrix and the accuracy of the model
-print("Confusion matrix:\n",confusion_matrix(test_data['CLASS'], predicted))
-print("Accuracy of the model on the test data: ", {np.mean(predicted == test_data['CLASS'])})
+y_pred = clf.predict(X_test)
+print("Confusion matrix:\n",confusion_matrix(y_test, y_pred))
+print("Accuracy of the model on the test data: ", accuracy_score(y_test, y_pred))
